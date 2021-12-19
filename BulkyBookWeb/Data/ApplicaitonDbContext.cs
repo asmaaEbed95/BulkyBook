1a91544b0ba8fb990e02a55cb03612d71fcaf78e
asmaa.ebed <asmaaebed95@gmail.com>
2021-12-19 11:04:40 +0200

    Add project files.

---
+++ BulkyBookWeb/Data/ApplicaitonDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookWeb.Data
+{
+    public class ApplicaitonDbContext : DbContext
+    {
+        public ApplicaitonDbContext(DbContextOptions<ApplicaitonDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
